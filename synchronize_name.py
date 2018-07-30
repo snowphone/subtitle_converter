c30b556067d0cc3748f6d543d8544b636bb530e5
Moon Junoh <mjo970625@gmail.com>
2018-07-30 17:19:53 +0900

    mkv, avi, mp4 지원

--- synchronize_name.py
+++ synchronize_name.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 
 import os
-from sys import argv
+import re
+from sys import argv, stderr
 
 def synchonize_name(path):
-	videos = [path + '/' + video for video in os.listdir(path) if video.find(".mkv") != -1]
-	subs = [path + '/' + sub for sub in os.listdir(path) if sub.find(".smi") != -1]
+	video_formats = ["mkv$", "avi$", "mp4$"]
+	video_pattern = re.compile("|".join(video_formats))
+	videos = [path + '/' + video for video in os.listdir(path) if  video_pattern.search(video)]
 
+	sub_pattern = re.compile("(smi|srt)$")
+	subs = [path + '/' + sub for sub in os.listdir(path) if sub_pattern.search(sub)]
 	for video, sub in zip(videos, subs):
 		new_sub = video.replace(".mkv", ".smi")
 		os.rename(sub, new_sub)
 		print(new_sub)
+
+
+if __name__ == "__main__":
+	try:
+		synchonize_name(argv[1])
+	except IndexError:
+		print("Usage: {} <path>".format(argv[0]))
